e9c2bc8f19c3017546d13e00b9bf7f1ce38b4608
Markus Ostermayer <markus.ostermayer@gmail.com>
2020-09-21 22:36:08 +0200

    Added a new function for better button-creation

--- utils.py
+++ utils.py
@@ -11,7 +11,9 @@ import logging
 import requests
 import sqlite3
 
+from telegram import InlineKeyboardButton
 from constants import Database as db_const
+from constants import TelegramConstants as tele_const
 
 
 def execute_query(connection, query):
@@ -41,24 +43,68 @@ def execute_query(connection, query):
     connection.commit()
     return result
 
+def region_cmd_buttons(sel_conn, query, cmd_prefix):
+    result = execute_query(sel_conn, query)
+
+    # If the result-tuple is empty, than there are not regions
+    # called the way the suer put it in
+    if(len(result) == 0):
+        return None
+    # If it is not empty, than we have regions we can print to the screen
+    # and let the user choose
+    else:
+        # list to store all buttons
+        region_keyboard = []
+
+        # itterate over the result, creating a button for each region
+        for item in result:
+            # create a command that is send back to the callbacl quary handler
+            # and gets prozessed there
+            command = "{cmd_prefix}_{name}_{id}".format(cmd_prefix=cmd_prefix,
+                name=str(item[1]), id=str(item[0]))
+
+            # with the command create the button with the name of the region
+            # as button text
+            button = InlineKeyboardButton(text=str(item[0]),
+                callback_data=command)
+
+            # append the button to the list of buttons
+            region_keyboard.append([button])
+
+        # add a button to cancel the current operation
+        button = InlineKeyboardButton(text=tele_const.CMD_PREFIX_CANCEL,
+            callback_data=tele_const.CMD_PREFIX_CANCEL)
+
+        # append the button to the list of buttons
+        region_keyboard.append([button])
+
+        # return the created list of buttons
+        return region_keyboard
 
 def simple_js_parser(pagecontent):
     """
     Simple parsing-function for parsing js-data from the corona-dashboard
     """
+    # create an empty dictionarry
     variables = {}
 
+    # get each line, stripping away any leading or ending newline
     lines = pagecontent.strip("\n").split("\n")
 
+    # iterate over the lines
     for line in lines:
+        # removing every var-word and replacing it with nothing, afterwards
+        # splitting by the =-Operator surrounded by two spaces
+        # TODO: Check if " = " nessesarry or if splitting by = and stripping
+        # works
         line_data = line.replace("var", "").split(" = ")
 
-        # print(line_data)
         variables[line_data[0].strip()] = ast.literal_eval(
             line_data[1].strip(";"))
+
     return variables
 
-
+# Keep the data in the cache, which will get reset every hour
 @lru_cache(maxsize=32)
 def get_data_js(url):
     """
